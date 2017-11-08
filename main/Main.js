ed6cd3532ec0d464c35118861047182c08fc7925
Vera Brockmeyer <vera.brockmeyer@googlemail.com>
2017-11-08 21:22:25 +0100

    init js files

---
+++ main/Main.js
@@ -0,0 +1,12 @@
+const TERMINALHEIGHT = 200;
+function setup(){
+var playground = createCanvas(windowWidth, windowHeight-TERMINALHEIGHT);
+    playground.parent("Playground");
+var terminal = createCanvas(windowWidth,TERMINALHEIGHT)
+    terminal.parent("Terminal");
+background(0);   
+}
+
+function draw(){
+  //TODO  
+}
