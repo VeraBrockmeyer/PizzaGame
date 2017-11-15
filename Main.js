c4d6e87e5a2f43341e9bdc1fcfcd697f6966129e
iammmai <dtmai@gmx.de>
2017-11-15 06:09:50 +0100

    Add Mushrooms

--- Main.js
+++ Main.js
@@ -9,12 +9,12 @@ function setup() {
 
 function draw() {
     image(bg, 0, 0, width, height)
-    
-    if(ingSys.ingredients.length < 10) {
+
+    if (ingSys.ingredients.length < 10) {
         ingSys.add()
     }
     console.log(ingSys.ingredients.length)
-    
+
     ingSys.run()
-    
+
 }
--- ingredients.js
+++ ingredients.js
@@ -1,5 +1,3 @@
-
-
 function Ingredient(x, y, radius) {
     this.loc = createVector(x, y)
     this.vel = createVector(0, 0)
@@ -33,27 +31,53 @@ Ingredient.prototype.isOut = function () {
     }
 }
 
+// Mushroom constructor, make Mushroom inherit from Ingredient
+
+function Mushroom (x,y,radius) {
+    Ingredient.call(this, x,y,radius)
+    this.img = loadImage("/assets/mushroom.png")
+    this.angle = random(-2*PI, 2*PI)
+}
+
+Mushroom.prototype = Object.create(Ingredient.prototype)
+Mushroom.prototype.constructor = Mushroom
+
+//overwrite render function
+Mushroom.prototype.render = function () {
+    push()
+    translate(this.loc.x, this.loc.y)
+    rotate(this.angle)
+    image(this.img, 0, 0, this.r, this.r)
+    pop()
+}
+
 
 // Ingredients system class
 
-function IngSystem () {
+function IngSystem() {
     this.ingredients = []
 }
 
 IngSystem.prototype.add = function () {
-    this.ingredients.push(new Ingredient(random(width), 0, random(20, 100)))
+    let x = random(1)
+    if (x > 0.5) {
+            this.ingredients.push(new Ingredient(random(width), 0, random(20, 100)))
+    } else {
+            this.ingredients.push(new Mushroom(random(width), 0, random(20, 100)))
+    }
+
 }
 
-IngSystem.prototype.run =function () {
-    for (let i = this.ingredients.length-1; i>= 0; i--) {
-        const gravitation = createVector(0,1)
+IngSystem.prototype.run = function () {
+    for (let i = this.ingredients.length - 1; i >= 0; i--) {
+        const gravitation = createVector(0, 1)
         let ing = this.ingredients[i]
         ing.applyForce(gravitation)
         ing.update()
         ing.render()
-        
-        if(ing.isOut()) {
-            this.ingredients.splice(i,1)
+
+        if (ing.isOut()) {
+            this.ingredients.splice(i, 1)
         }
     }
 }
